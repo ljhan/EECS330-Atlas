c80258e17b5bc845d0e592c2a484cdb8758e1dda
Alyssa <alyssaliu2018@u.northwestern.edu>
2017-03-09 00:59:47 -0600

    Update explore2.js

--- js/explore2.js
+++ js/explore2.js
@@ -10,15 +10,76 @@ function closeFriendsList(){
 }
 
 // MAPS //
+var John_S = '<div id="iw-container">' +
+                      '<div class="iw-content">' +
+                     '<img src="male1.png">' +
+                     '<div class="iw-subTitle">John S.</div>' +
+                     '</div>'+
+                  '<p class="now_playing"><i>(now listening)</i></p>'+
+                  '<p class="song_info"><b>♪ Caroline</b> by Animé</p>' +
+                 '</div>';
+var Sandy_P = '<div id="iw-container">' +
+                      '<div class="iw-content">' +
+                     '<img src="female1.png">' +
+                     '<div class="iw-subTitle">Sandy P.</div>' +
+                     '</div>'+
+                  '<p class="now_playing"><i>(now listening)</i></p>'+
+                  '<p class="song_info"><b>♪ Starboy</b> by The Weeknd</p>' +
+                 '</div>';
+var Lucas_G = '<div id="iw-container">' +
+                      '<div class="iw-content">' +
+                     '<img src="male2.png">' +
+                     '<div class="iw-subTitle">Lucas G.</div>' +
+                     '</div>'+
+                  '<p class="now_playing"><i>(now listening)</i></p>'+
+                  '<p class="song_info"><b>♪ Float On</b> by Modest Mouse</p>' +
+                 '</div>';
+
+var Kelsey_M = '<div id="iw-container">' +
+                      '<div class="iw-content">' +
+                      '<img src="female2.png">' +
+                      '<div class="iw-subTitle">Kelsey M.</div>' +
+                          '</div>'+
+                      '<p class="now_playing"><i>(now listening)</i></p>'+
+                      '<p class="song_info"><b>♪ Primordial Sound</b> by The Contortionist</p>' +
+                  '</div>';
+
+var Barry_C= '<div id="iw-container">' +
+'<div class="iw-content">' +
+'<img src="male3.png">' +
+'<div class="iw-subTitle">Barry C.</div>' +
+'</div>'+
+'<p class="now_playing"><i>(now listening)</i></p>'+
+'<p class="song_info"><b>♪ Bad Things</b> by Machine Gun Kelly</p>' +
+'</div>';
+
+var Drew_C= '<div id="iw-container">' +
+'<div class="iw-content">' +
+'<img src="male4.png">' +
+'<div class="iw-subTitle">Drew C.</div>' +
+'</div>'+
+'<p class="now_playing"><i>(now listening)</i></p>'+
+'<p class="song_info"><b>♪ Paris</b> by The Chainsmokers</p>' +
+'</div>';
+
+var Kent_R= '<div id="iw-container">' +
+'<div class="iw-content">' +
+'<img src="male5.png">' +
+'<div class="iw-subTitle">Kent R.</div>' +
+'</div>'+
+'<p class="now_playing"><i>(now listening)</i></p>'+
+'<p class="song_info"><b>♪ Fake Love</b> by Drake</p>' +
+'</div>';
+
 
  var locations = [
-      ['Olivia: ♪ Float On', 42.057996, -87.676640, 7],
-      ['Alyssa: ♪ Primordial Sound', 42.057799, -87.676194, 6],
-      ['Xuan: ♪ Starboy', 42.057995, -87.675918, 5],
-      ['Evan: ♪ Caroline', 42.057916, -87.675551, 4], 
-      ['Alice: ♪ Paris', 42.058791, -87.675463, 3],
-      ['Barry: ♪ Bad Things', 42.058066, -87.674444, 2],
-      ['Cory: ♪ Fake Love', 42.057548, -87.673618, 1]
+   [Kelsey_M, 42.057996, -87.676640, 7],
+   [Lucas_G, 42.057799, -87.676194, 6],
+   [Sandy_P, 42.057995, -87.675918, 5],
+   [John_S, 42.057916, -87.675551, 4],
+   [Drew_C, 42.058791, -87.675463, 3],
+   [Barry_C, 42.058066, -87.674444, 2],
+    [Kent_R, 42.057548, -87.673618, 1]
     ];
 
     var map = new google.maps.Map(document.getElementById('explore-map'), {
@@ -44,10 +105,20 @@ function closeFriendsList(){
         icon: image
       });
 
-      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+      google.maps.event.addListener(marker, 'mouseover', (function(marker, i) {
         return function() {
           infowindow.setContent(locations[i][0]);
           infowindow.open(map, marker);
         }
       })(marker, i));
     }
+
+var carolineLoc = new google.maps.LatLng(42.057916, -87.675551);
+
+function findCaroline() {
+  infowindow.setContent(locations[6][0]);
+  infowindow.open(map, marker);
+  map.setCenter(carolineLoc);
+  map.setZoom(24);
+
+}
